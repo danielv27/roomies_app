277f0d9e5bfed93c7c1d7ab594a508a6dd756dd1
Daniel Verner <danielv2000@gmail.com>
2022-05-01 14:34:42 +0200

    add font logo and spacing to the login page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
+import 'pages/login_page.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -45,96 +46,12 @@ class MainPage extends StatelessWidget {
          return HomePage();
        }
        else {
-         return Container(child: LoginWidget(), margin: const EdgeInsets.only(top: 200));
+         return LoginPage();
        }
      }
     ),
   );
 }
-
-class LoginWidget extends StatefulWidget {
-  @override
-  LoginWidgetState createState() => LoginWidgetState();
-}
-
-
-class LoginWidgetState extends State<LoginWidget> {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-
-  bool invalidUserName = false;
-  bool invalidPassword = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(15),
-      child: Column(children: [
-        const SizedBox(height: 20),
-        TextField(
-          controller: emailController,
-          textInputAction: TextInputAction.next,
-          decoration: InputDecoration(
-            icon: const Icon(Icons.email,size: 20),
-            
-            labelText: "Email",
-            errorText: invalidUserName ? "Invalid Email" : null
-            ),
-        ),
-        const SizedBox(height: 4),
-        TextField(
-          controller: passwordController,
-          textInputAction: TextInputAction.done,
-          obscureText: true,
-          decoration: InputDecoration(
-            icon: const Icon(Icons.lock,size: 20),
-            labelText: "Password",
-            errorText: invalidPassword ? "Invalid Password" : null
-            ),
-        ),
-        
-        const SizedBox(height: 20),
-        
-        ElevatedButton.icon(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.redAccent[400],
-            minimumSize: const Size.fromHeight(50)
-            
-          ),
-          icon: const Icon(Icons.lock_open, size: 32),
-          label: const Text("Log In", style: TextStyle(fontSize: 24)), 
-          onPressed: signIn,
-          ),
-      ]),
-    );
-  }
-  
-  Future signIn() async {
-    try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: emailController.text.trim(), 
-        password: passwordController.text.trim(),
-      );
-
-    } on FirebaseAuthException catch (exc) {
-      setState(() {
-        if (exc.toString().contains('email') || exc.toString().contains('user')){
-          invalidUserName = true;
-        }
-        if (exc.toString().contains('password')){
-          invalidPassword = true;
-        }
-        // ignore: avoid_print
-        print(exc.toString());
-      });
-
-    }
-
-  }
-}
-
-
-
 
 
 
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children:[
+          const SizedBox(height: 110,),
+          const Image(image: AssetImage('assets/images/demo-house.png'),height: 120,),
+          Text(
+            'Roomies',
+            style: TextStyle(fontFamily: 'BebasNeue', fontSize: 32)
+          ),
+          const SizedBox(height: 22,),
+          LoginWidget()
+        ],
+      )
+    );
+  }
+}
+
+class LoginWidget extends StatefulWidget {
+  @override
+  LoginWidgetState createState() => LoginWidgetState();
+}
+
+
+class LoginWidgetState extends State<LoginWidget> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  bool invalidUserName = false;
+  bool invalidPassword = false;
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: LoginWidget(), margin: const EdgeInsets.only(top: 200));
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(15),
+      child: Column(children: [
+        const SizedBox(height: 20),
+        TextField(
+          controller: emailController,
+          textInputAction: TextInputAction.next,
+          decoration: InputDecoration(
+            icon: const Icon(Icons.email,size: 20),
+            
+            labelText: "Email",
+            errorText: invalidUserName ? "Invalid Email" : null
+            ),
+        ),
+        const SizedBox(height: 4),
+        TextField(
+          controller: passwordController,
+          textInputAction: TextInputAction.done,
+          obscureText: true,
+          decoration: InputDecoration(
+            icon: const Icon(Icons.lock,size: 20),
+            labelText: "Password",
+            errorText: invalidPassword ? "Invalid Password" : null
+            ),
+        ),
+        
+        const SizedBox(height: 20),
+        
+        ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.redAccent[400],
+            minimumSize: const Size.fromHeight(50)
+            
+          ),
+          icon: const Icon(Icons.lock_open, size: 32),
+          label: const Text("Log In", style: TextStyle(fontSize: 24)), 
+          onPressed: signIn,
+          ),
+      ]),
+    );
+  }
+  
+  Future signIn() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(), 
+        password: passwordController.text.trim(),
+      );
+
+    } on FirebaseAuthException catch (exc) {
+      setState(() {
+        if (exc.toString().contains('email') || exc.toString().contains('user')){
+          invalidUserName = true;
+        }
+        if (exc.toString().contains('password')){
+          invalidPassword = true;
+        }
+        // ignore: avoid_print
+        print(exc.toString());
+      });
+
+    }
+
   }
 }
