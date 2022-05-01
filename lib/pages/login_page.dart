fa7ab6e3d9384818a2e981e5f9223368e9d8b6c2
Daniel Verner <danielv2000@gmail.com>
2022-05-01 16:05:52 +0200

    fix??

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,7 +6,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomInset: true,
       body: SingleChildScrollView(
         child: Column(
           children:[
