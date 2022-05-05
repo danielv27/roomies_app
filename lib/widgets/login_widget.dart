f49003c7d69417e3df0503a9da640ecbc8253f7c
dvolpin29 <d.volpin@student.vu.nl>
2022-05-05 19:38:03 +0200

    added password visibility feature

--- lib/widgets/login_widget.dart
+++ lib/widgets/login_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../pages/signup_page.dart';
@@ -16,11 +17,11 @@ class LoginWidget extends StatefulWidget {
   LoginWidgetState createState() => LoginWidgetState();
 }
 
-
 class LoginWidgetState extends State<LoginWidget> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   bool _isChecked = false;
+  bool _isHiddrenPassword = true;
 
   @override
   void initState() {
@@ -45,64 +46,77 @@ class LoginWidgetState extends State<LoginWidget> {
       padding: const EdgeInsets.all(30),
       child: Column(children: [
         const Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-                "Welcome!",
-                style: TextStyle(fontWeight: FontWeight.bold ,fontSize: 26),
-            ),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            "Welcome!",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
           ),
+        ),
+        const SizedBox(height: 10),
         TextField(
           controller: emailController,
           style: TextStyle(color: Colors.grey),
           cursorColor: Colors.grey,
           textInputAction: TextInputAction.next,
           decoration: InputDecoration(
-            focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
-            icon: const Icon(Icons.email,size: 20,color: Colors.grey,),
-            iconColor: Colors.grey,
-            fillColor: Colors.grey,
-            labelText: "Email",
-            labelStyle: const TextStyle(color: Colors.grey),
-            errorText: invalidUserName ? "Invalid Email" : null
-            ),
+              focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey)),
+              icon: const Icon(
+                Icons.email,
+                size: 20,
+                color: Colors.grey,
+              ),
+              iconColor: Colors.grey,
+              fillColor: Colors.grey,
+              labelText: "Email",
+              labelStyle: const TextStyle(color: Colors.grey),
+              errorText: invalidUserName ? "Invalid Email" : null),
         ),
-        const SizedBox(height: 4),
+        const SizedBox(height: 6),
         TextField(
           controller: passwordController,
           style: TextStyle(color: Colors.grey),
           cursorColor: Colors.red,
           textInputAction: TextInputAction.done,
-          obscureText: true,
+          obscureText: _isHiddrenPassword,
           decoration: InputDecoration(
             //the lines below change the color of the underline of the text field
             // enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
-            focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
-            icon: const Icon(Icons.lock,size: 20, color: Colors.grey,),
+            focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey)),
+            icon: const Icon(
+              Icons.lock,
+              size: 20,
+              color: Colors.grey,
+            ),
             labelText: "Password",
             labelStyle: const TextStyle(color: Colors.grey),
-            errorText: invalidPassword ? "Invalid Password" : null
-            
+            errorText: invalidPassword ? "Invalid Password" : null,
+            suffixIcon: InkWell(
+              onTap: _togglePasswordView,
+              child: const Icon(
+                Icons.visibility,
+              ),
             ),
+          ),
         ),
         Row(mainAxisAlignment: MainAxisAlignment.start, children: [
           SizedBox(
-              height: 24.0,
-              width: 24.0,
-              child: Theme(
-                data: ThemeData(
-                    unselectedWidgetColor: Color(0xff00C8E8) // Your color
-                ),
-                child: Checkbox(
-                    activeColor: Color(0xff00C8E8),
-                    value: _isChecked,
-                    onChanged: _handleRemember),
-              )),
+            height: 24.0,
+            width: 24.0,
+            child: Theme(
+              data: ThemeData(unselectedWidgetColor: (Colors.grey)),
+              child: Checkbox(
+                activeColor: Colors.red,
+                value: _isChecked,
+                onChanged: _handleRemember),
+            ),
+          ),
           const SizedBox(width: 10.0),
-          const Text("Remember Me",
-              style: TextStyle(
-                  color: Color(0xff646464),
-                  fontSize: 12,
-                  fontFamily: 'Rubic'))
+          const Text(
+            "Remember Me",
+            style: TextStyle(color: Color(0xff646464), fontSize: 12, fontFamily: 'Rubic'),
+          ),
         ]),
         const SizedBox(height: 20),
         Container(
@@ -110,38 +124,47 @@ class LoginWidgetState extends State<LoginWidget> {
           margin: EdgeInsets.only(bottom: 10),
           child: ElevatedButton.icon(
             style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0)),
               shadowColor: Colors.red[900],
               elevation: 5,
               primary: Colors.white,
-              minimumSize: const Size.fromHeight(42)
+              minimumSize: const Size.fromHeight(42),
             ),
-            icon: const Icon(Icons.lock_open, size: 24,color: Colors.red,),
-            label: const Text("Log In", style: TextStyle(fontSize: 20, color: Colors.red)), 
+            icon: const Icon(
+              Icons.lock_open,
+              size: 24,
+              color: Colors.red,
+            ),
+            label: const Text("Log In",
+              style: TextStyle(fontSize: 20, color: Colors.red)),
             onPressed: signIn,
-            ),
+          ),
         ),
         RichText(
           text: TextSpan(
-            text: 'Don\'t have account? ', style: const TextStyle(
+            text: 'Don\'t have account? ',
+            style: const TextStyle(
               color: Colors.grey,
             ),
             children: [
               TextSpan(
-              recognizer: TapGestureRecognizer()..onTap = () =>
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignupPage()),
-                  ),
-              style: const TextStyle(
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignupPage()),
+                    );
+                  },
+                style: const TextStyle(
                   decoration: TextDecoration.underline,
                   color: Colors.blue,
                   backgroundColor: Colors.white
                 ),
                 text: 'Sign Up'
-              )
+              ),
             ],
-          )
+          ),
         ),
       ]),
     );
@@ -153,13 +176,13 @@ class LoginWidgetState extends State<LoginWidget> {
         email: emailController.text.trim(),
         password: passwordController.text.trim(),
       );
-
     } on FirebaseAuthException catch (exc) {
       setState(() {
-        if (exc.toString().contains('email') || exc.toString().contains('user')){
+        if (exc.toString().contains('email') ||
+            exc.toString().contains('user')) {
           invalidUserName = true;
         }
-        if (exc.toString().contains('password')){
+        if (exc.toString().contains('password')) {
           invalidPassword = true;
         }
         // ignore: avoid_print
@@ -172,7 +195,7 @@ class LoginWidgetState extends State<LoginWidget> {
     print("Handle Remember Me");
     _isChecked = checkboxState!;
     SharedPreferences.getInstance().then(
-          (prefs) {
+      (prefs) {
         prefs.setBool("remember_me", checkboxState);
         prefs.setString('email', emailController.text);
         prefs.setString('password', passwordController.text);
@@ -194,6 +217,7 @@ class LoginWidgetState extends State<LoginWidget> {
       print(_rememberMe);
       print(_email);
       print(_password);
+
       if (_rememberMe) {
         setState(() {
           _isChecked = true;
@@ -205,5 +229,11 @@ class LoginWidgetState extends State<LoginWidget> {
       print(e);
     }
   }
-}
 
+  void _togglePasswordView() {
+    setState(() {
+      _isHiddrenPassword = !_isHiddrenPassword;
+    });
+  }
+  
+}
