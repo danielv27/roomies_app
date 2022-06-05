ec1578ee4b967bd3ad9764d785a56a7e59ad23cb
Daniel Verner <danielv2000@gmail.com>
2022-06-05 22:57:43 +0200

    added seethrough likedislike bar and fixed image alignments of users

--- lib/pages/roomies_page.dart
+++ lib/pages/roomies_page.dart
@@ -65,8 +65,7 @@ class RoomiesPage extends StatelessWidget {
       body: Column(
           children: [
             imageRoomiesInfo(context),
-            const SizedBox(height: 15),
-            likeDislikeBar(context),
+            //likeDislikeBar(context),
           ],
         ),
   );
@@ -75,11 +74,8 @@ class RoomiesPage extends StatelessWidget {
 
   SizedBox imageRoomiesInfo(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.575,
-      width: MediaQuery.of(context).size.width * 0.92,
+      height: MediaQuery.of(context).size.height * 0.72,
       child: SwipableCards(),
-
-
 
       // child: Stack(
       //   children: <Widget>[
@@ -107,155 +103,155 @@ class RoomiesPage extends StatelessWidget {
     );
   }
 
-  Row likeDislikeBar(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        SizedBox.fromSize(
-          size: const Size(56, 56),
-          child: ClipOval(
-            child: Material(
-              color: Colors.white,
-              child: InkWell(
-                splashColor: Colors.red[50],
-                onTap: () {
-                  print("Dislike button pressed");
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const <Widget>[
-                    ImageIcon(
-                      AssetImage("assets/icons/Close.png"),
-                      color: Colors.red,
-                      size: 26,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(width: 30),    
-        SizedBox.fromSize(
-          size: const Size(70, 70),
-          child: ClipOval(
-            child: Material(
-              color: Colors.red,
-              child: InkWell(
-                splashColor: Colors.red[50],
-                onTap: () {
-                  print("Like Button pressed");
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const <Widget>[
-                    ImageIcon(
-                      AssetImage("assets/icons/Heart.png"),
-                      color: Colors.white,
-                      size: 30,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(width: 30),
-        SizedBox.fromSize(
-          size: const Size(56, 56),
-          child: ClipOval(
-            child: Material(
-              color: Colors.white,
-              child: InkWell(
-                splashColor: Colors.red[50],
-                onTap: () {
-                  print("Info button pressed");
-                  showUserInfo(context);
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const <Widget>[
-                    ImageIcon(
-                      AssetImage("assets/icons/Info.png"),
-                      color: Color.fromARGB(255, 116, 201, 175),
-                      size: 30,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
+  // Row likeDislikeBar(BuildContext context) {
+  //   return Row(
+  //     mainAxisAlignment: MainAxisAlignment.center,
+  //     children: <Widget>[
+  //       SizedBox.fromSize(
+  //         size: const Size(56, 56),
+  //         child: ClipOval(
+  //           child: Material(
+  //             color: Colors.white,
+  //             child: InkWell(
+  //               splashColor: Colors.red[50],
+  //               onTap: () {
+  //                 print("Dislike button pressed");
+  //               },
+  //               child: Column(
+  //                 mainAxisAlignment: MainAxisAlignment.center,
+  //                 children: const <Widget>[
+  //                   ImageIcon(
+  //                     AssetImage("assets/icons/Close.png"),
+  //                     color: Colors.red,
+  //                     size: 26,
+  //                   ),
+  //                 ],
+  //               ),
+  //             ),
+  //           ),
+  //         ),
+  //       ),
+  //       const SizedBox(width: 30),    
+  //       SizedBox.fromSize(
+  //         size: const Size(70, 70),
+  //         child: ClipOval(
+  //           child: Material(
+  //             color: Colors.red,
+  //             child: InkWell(
+  //               splashColor: Colors.red[50],
+  //               onTap: () {
+  //                 print("Like Button pressed");
+  //               },
+  //               child: Column(
+  //                 mainAxisAlignment: MainAxisAlignment.center,
+  //                 children: const <Widget>[
+  //                   ImageIcon(
+  //                     AssetImage("assets/icons/Heart.png"),
+  //                     color: Colors.white,
+  //                     size: 30,
+  //                   ),
+  //                 ],
+  //               ),
+  //             ),
+  //           ),
+  //         ),
+  //       ),
+  //       const SizedBox(width: 30),
+  //       SizedBox.fromSize(
+  //         size: const Size(56, 56),
+  //         child: ClipOval(
+  //           child: Material(
+  //             color: Colors.white,
+  //             child: InkWell(
+  //               splashColor: Colors.red[50],
+  //               onTap: () {
+  //                 print("Info button pressed");
+  //                 showUserInfo(context);
+  //               },
+  //               child: Column(
+  //                 mainAxisAlignment: MainAxisAlignment.center,
+  //                 children: const <Widget>[
+  //                   ImageIcon(
+  //                     AssetImage("assets/icons/Info.png"),
+  //                     color: Color.fromARGB(255, 116, 201, 175),
+  //                     size: 30,
+  //                   ),
+  //                 ],
+  //               ),
+  //             ),
+  //           ),
+  //         ),
+  //       ),
+  //     ],
+  //   );
+  // }
 
-  void showUserInfo(BuildContext context) {
-    showModalBottomSheet<void>(
-      barrierColor: Colors.transparent,
-      isScrollControlled: true,
-      context: context,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(30.0), 
-          topRight: Radius.circular(30.0)
-        ),
-      ),
-      builder: (BuildContext context) {
-        return SingleChildScrollView(
-          padding: MediaQuery.of(context).viewInsets,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.all(15),
-                  child: Column(
-                    children: [
-                      const Text("Daniel Volpin"),
-                      const Divider(
-                        color: Color.fromARGB(255, 163, 163, 163),
-                      ),
-                      const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "About",
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
-                        ),
-                      ),
-                      const Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.. Readmore"),
-                      const SizedBox(height: 12,),
-                      const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Work",
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
-                        ),
-                      ),
-                      const Text("Wonen op een prachtige locatie in de Pijp! Dit ruim appartement is onderdeel van een goed..."),
-                      const SizedBox(height: 12,),
-                      const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Study",
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
-                        ),
-                      ),
-                      const Text("Wonen op een prachtige locatie in de Pijp! Dit ruim appartement is onderdeel van .."),
-                      const SizedBox(height: 20),
-                      Row(),
-                      const SizedBox(height: 200),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
+  // void showUserInfo(BuildContext context) {
+  //   showModalBottomSheet<void>(
+  //     barrierColor: Colors.transparent,
+  //     isScrollControlled: true,
+  //     context: context,
+  //     shape: const RoundedRectangleBorder(
+  //       borderRadius: BorderRadius.only(
+  //         topLeft: Radius.circular(30.0), 
+  //         topRight: Radius.circular(30.0)
+  //       ),
+  //     ),
+  //     builder: (BuildContext context) {
+  //       return SingleChildScrollView(
+  //         padding: MediaQuery.of(context).viewInsets,
+  //         child: Center(
+  //           child: Column(
+  //             mainAxisAlignment: MainAxisAlignment.center,
+  //             mainAxisSize: MainAxisSize.min,
+  //             children: <Widget>[
+  //               Container(
+  //                 padding: const EdgeInsets.all(15),
+  //                 child: Column(
+  //                   children: [
+  //                     const Text("Daniel Volpin"),
+  //                     const Divider(
+  //                       color: Color.fromARGB(255, 163, 163, 163),
+  //                     ),
+  //                     const Align(
+  //                       alignment: Alignment.centerLeft,
+  //                       child: Text(
+  //                         "About",
+  //                         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+  //                       ),
+  //                     ),
+  //                     const Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.. Readmore"),
+  //                     const SizedBox(height: 12,),
+  //                     const Align(
+  //                       alignment: Alignment.centerLeft,
+  //                       child: Text(
+  //                         "Work",
+  //                         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+  //                       ),
+  //                     ),
+  //                     const Text("Wonen op een prachtige locatie in de Pijp! Dit ruim appartement is onderdeel van een goed..."),
+  //                     const SizedBox(height: 12,),
+  //                     const Align(
+  //                       alignment: Alignment.centerLeft,
+  //                       child: Text(
+  //                         "Study",
+  //                         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+  //                       ),
+  //                     ),
+  //                     const Text("Wonen op een prachtige locatie in de Pijp! Dit ruim appartement is onderdeel van .."),
+  //                     const SizedBox(height: 20),
+  //                     Row(),
+  //                     const SizedBox(height: 200),
+  //                   ],
+  //                 ),
+  //               ),
+  //             ],
+  //           ),
+  //         ),
+  //       );
+  //     },
+  //   );
+  // }
 
   // Widget buildUsers(dataList) {
   //   return SizedBox(
--- lib/widgets/swipable_cards.dart
+++ lib/widgets/swipable_cards.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:swipable_stack/swipable_stack.dart';
+import '../widgets/like_dislike_bar.dart';
 
 const _images = [
-  'assets/images/profile_pic.png',
   'assets/images/profile_pic2.jpg',
   'assets/images/profile_pic3.jpg',
   'assets/images/profile_pic4.jpg',
   'assets/images/profile_pic5.jpg',
-
 ];
 
 class SwipableCards extends StatefulWidget {
@@ -37,17 +36,44 @@ class SwipableCardsState extends State<SwipableCards> {
 
   @override
   Widget build(BuildContext context) {
-    return SwipableStack(
-      controller: _controller,
-      builder: (context, properties) {
-        //return Image.asset(_images[1]);
-        final itemIndex = properties.index % _images.length;
-        return Stack(
-          children: [
-            Image.asset(_images[itemIndex])
-          ],
-        );
-      },
+    return Stack(
+      children:
+      [
+        SwipableStack(
+          controller: _controller,
+          builder: (context, properties) {
+            final itemIndex = properties.index % _images.length;
+            return Stack(
+            
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.72,
+                  child: Image.asset(_images[itemIndex],fit: BoxFit.fitHeight)
+                ),
+                const Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 120),
+                    child: Text(
+                      "Daniel Volpin", 
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32
+                      )
+                    ),
+                  )
+                )
+              ],
+            );
+          },
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: likeDislikeBar(context),
+          
+        ),
+      ]
     );
   }
 }
