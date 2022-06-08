d8402eb0fc8949f9a01b99634c5f4fccddc184d8
Daniel Verner <danielv2000@gmail.com>
2022-06-08 19:28:55 +0200

    tried to fix multiple users on swipable cards

--- lib/widgets/swipable_cards.dart
+++ lib/widgets/swipable_cards.dart
@@ -2,6 +2,29 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:swipable_stack/swipable_stack.dart';
 import '../widgets/like_dislike_bar.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+const userImageArray = [
+  [
+    'assets/images/profile_pic2.jpg',
+    'assets/images/profile_pic3.jpg',
+    'assets/images/profile_pic4.jpg',
+    'assets/images/profile_pic5.jpg',
+  ],
+  [
+    'assets/images/profile_pic3.jpg',
+    'assets/images/profile_pic2.jpg',
+    'assets/images/profile_pic4.jpg',
+    'assets/images/profile_pic5.jpg',
+  ],
+  [
+    'assets/images/profile_pic4.jpg',
+    'assets/images/profile_pic5.jpg',
+  ],
+  [
+    'assets/images/profile_pic5.jpg',
+  ]
+];
 
 const _images = [
   'assets/images/profile_pic2.jpg',
@@ -16,20 +39,23 @@ class SwipableCards extends StatefulWidget {
 }
 
 class SwipableCardsState extends State<SwipableCards> {
-  late final SwipableStackController _controller;
+  late final SwipableStackController swipeController; // = SwipableStackController()..addListener(_listenController);
+  late final imageController = PageController(viewportFraction: 1,keepPage: true);
+  
 
   void _listenController() => setState(() {});
 
   @override
   void initState() {
     super.initState();
-    _controller = SwipableStackController()..addListener(_listenController);
+    swipeController = SwipableStackController()..addListener(_listenController);
   }
 
   @override
   void dispose() {
     super.dispose();
-    _controller
+    
+    swipeController
       ..removeListener(_listenController)
       ..dispose();
   }
@@ -40,20 +66,64 @@ class SwipableCardsState extends State<SwipableCards> {
       children:
       [
         SwipableStack(
-          controller: _controller,
+          onWillMoveNext: (index, direction) {
+            final allowedActions = [
+            SwipeDirection.right,
+            SwipeDirection.left,
+            
+            ];
+            return allowedActions.contains(direction);
+          },
+          controller: swipeController,
           onSwipeCompleted: (index, direction) {
             //this is where a swipe is handled
             print("current index: $index,\ndirection: $direction\n");
           },
           builder: (context, properties) {
-            final itemIndex = properties.index % _images.length;
+            final currentUserIndex = properties.index % _images.length;
             return Stack(
               children: [
                 SizedBox(
                   width: MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.height * 0.72,
-                  child: Image.asset(_images[itemIndex],fit: BoxFit.fitHeight)
+                  //child: Image.asset(_images[currentUserIndex],fit: BoxFit.fitHeight),
+                  child: PageView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    controller: imageController,
+                    itemBuilder: (context, index){
+                      return GestureDetector(
+                        child: Image.asset(userImageArray[currentUserIndex][index],fit: BoxFit.fitHeight),
+                        onTapDown:(TapDownDetails details) {
+                          var deviceWidth = MediaQuery.of(context).size.width;
+                          var xPos = details.globalPosition.dx;
+                          print(MediaQuery.of(context).size.width);
+                          print("tap on " + xPos.toString());
+                          if(xPos > deviceWidth * 0.65){
+                            imageController.nextPage(duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
+                            //index++;
+                            print(index);
+
+                          }
+                        },                        
+                      ); // return Image.asset(_images[index],fit: BoxFit.fitHeight);
+                    },
+                  ),
                 ),
+                Align(
+                alignment: Alignment.topCenter,
+                child: SmoothPageIndicator(
+                controller: imageController,
+                count: userImageArray[currentUserIndex].length,
+                effect: const ScrollingDotsEffect(
+                  dotHeight: 12,
+                  dotWidth: 30,
+                  
+                  activeDotColor: Colors.white,
+                  activeDotScale: 1.1
+                ),
+              ),
+                ),
+
                 const Align(
                   alignment: Alignment.bottomLeft,
                   child: Padding(
@@ -81,17 +151,11 @@ class SwipableCardsState extends State<SwipableCards> {
           // },
           //add this line to remove the ability to move the image around
           //allowVerticalSwipe: false,
-          onWillMoveNext: (index, direction) {
-            final allowedActions = [
-            SwipeDirection.right,
-            SwipeDirection.left,
-            ];
-            return allowedActions.contains(direction);
-          },
+
         ),
         Align(
           alignment: Alignment.bottomCenter,
-          child: likeDislikeBar(context, _controller),
+          child: likeDislikeBar(context, swipeController),
           
         ),
       ]
