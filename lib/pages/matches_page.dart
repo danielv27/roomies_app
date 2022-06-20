37541bd8788921d3d112bfed8b04534b22abe0c6
Daniel Verner <danielv2000@gmail.com>
2022-06-20 19:59:27 +0200

    Added searchbar to chatpage and cleaned up code

--- lib/pages/matches_page.dart
+++ lib/pages/matches_page.dart
@@ -12,27 +12,39 @@ class MatchesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: FutureBuilder(
-          future: FireStoreDataBase().getUsers(),
-          builder: (context, snapshot) {
-            if(snapshot.connectionState == ConnectionState.done){
-              List<UserModel> userList = snapshot.data as List<UserModel>;
-              return Column(
-                children: [
-                  MatchesHeaderWidget(users: userList),
-                  MatchesBodyWidget(users: userList)
-                ] 
-              );             
-            }
-            if (snapshot.hasError) {
-              return const Text("Something went wrong");
-            }
-            return const Center(child: CircularProgressIndicator(color: Colors.red));
-
-          },
+      body: Container(
+          decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+            Color.fromRGBO(239, 85, 100, 1),
+            Color.fromRGBO(195, 46, 66, 1),
+            Color.fromRGBO(190, 40, 62, 1),
+            Color.fromRGBO(210, 66, 78, 1),
+            Color.fromRGBO(244, 130, 114, 1),
+          ])),
+          child: FutureBuilder(
+            future: FireStoreDataBase().getUsers(),
+            builder: (context, snapshot) {
+              if(snapshot.connectionState == ConnectionState.done){
+                List<UserModel> userList = snapshot.data as List<UserModel>;
+                return Column(
+                  children: [
+                    MatchesHeaderWidget(users: userList),
+                    MatchesBodyWidget(users: userList)
+                  ] 
+                );             
+              }
+              if (snapshot.hasError) {
+                return const Text("Something went wrong");
+              }
+              return const Center(child: CircularProgressIndicator(color: Colors.red));
+        
+            },
+          ),
         ),
-      ),
+      
     );
   }
 }
--- lib/widgets/matches_page/matches_header.dart
+++ lib/widgets/matches_page/matches_header.dart
@@ -10,66 +10,87 @@ class MatchesHeaderWidget extends StatelessWidget {
     }) : super(key: key);
   @override
   Widget build(BuildContext context){
-    return Container(
-      decoration: const BoxDecoration(
-      gradient: LinearGradient(
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-        colors: [
-        Color.fromRGBO(239, 85, 100, 1),
-        Color.fromRGBO(195, 46, 66, 1),
-        Color.fromRGBO(190, 40, 62, 1),
-        Color.fromRGBO(210, 66, 78, 1),
-        Color.fromRGBO(244, 130, 114, 1),
-    ])),
-      child: Padding(
-        padding: const EdgeInsets.only(left:16.0,top:30),
-        child: Column(
-          children: [
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Hello",
-                style: TextStyle(
-                  color: Colors.white
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "${users[1].firstName} ${users[1].lastName}",
-                style: TextStyle(
-                  color: Colors.white
-                ),
-              ),
-            ),
-            SizedBox(
-                height: 200,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: users.length,
-                  itemBuilder: (context, index){
-                    return Padding(
-                      padding: const EdgeInsets.only(right:11.0,top: 11),
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: Colors.red,
-                            radius: 32,
-                            backgroundImage: NetworkImage(users[index].firstImgUrl),
-                          ),
-                          Text(users[index].firstName, style: TextStyle(color: Colors.white),)
-                        ],
-                      ),
-                    );
-                  }
-                  ),
-              
-            ),
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.only(top:32),
+      child: Column(
+        children: [
+          searchBar(users),
+          SizedBox(height: MediaQuery.of(context).size.height*0.02),
+          circularUserList(context, users),
+        ],
       ),
     );
   }
+}
+
+Widget searchBar(List<UserModel> users){
+  return AppBar(
+    centerTitle: false,
+    backgroundColor: Colors.transparent,
+    shadowColor: Colors.transparent,
+    actions: [
+        CircleAvatar(
+        radius: 32,
+        backgroundColor: Colors.white.withOpacity(0.4),
+        child: const Icon(Icons.search_rounded,color: Colors.white,),
+      ),
+      CircleAvatar(
+        backgroundColor: Colors.red,
+        radius: 32,
+        backgroundImage: NetworkImage(users[1].firstImgUrl),
+      ),
+      const Padding(padding: EdgeInsets.only(right: 20))
+
+    ],
+    title: Column(
+      children: [
+        const Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            "Good night,",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.white
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            "${users[1].firstName} ${users[1].lastName}",
+            style: const TextStyle(
+              //fontSize: 16,
+              color: Colors.white
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget circularUserList(BuildContext context, List<UserModel> users){
+  return SizedBox(
+    height: MediaQuery.of(context).size.height *0.2,
+    child: ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: users.length,
+      itemBuilder: (context, index){
+        return Padding(
+          padding: const EdgeInsets.only(right:11.0,top: 11),
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 32,
+                backgroundImage: NetworkImage(users[index].firstImgUrl),
+              ),
+              Text(users[index].firstName, style: TextStyle(color: Colors.white),)
+            ],
+          ),
+        );
+      }
+    ), 
+  );
 }
