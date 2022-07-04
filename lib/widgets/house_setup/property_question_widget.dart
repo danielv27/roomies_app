2126e1d8e2e87ca1ce969635ed2a3e718a35094d
dvolpin29 <d.volpin@student.vu.nl>
2022-07-04 17:42:58 +0200

    created property type list view in house setup

--- lib/widgets/house_setup/property_question_widget.dart
+++ lib/widgets/house_setup/property_question_widget.dart
@@ -3,53 +3,63 @@ import 'package:flutter/material.dart';
 import '../circle_ring.dart';
 
 class PropertyQuestionPage extends StatelessWidget {
-  const PropertyQuestionPage({
+  PropertyQuestionPage({
     Key? key,
   }) : super(key: key);
 
+  final List propertyTypeList = ["Apartment", "Studio", "Studentencomplex", "Tussenwoning", "Hoekwoning", "2-onder-1-kapwoning", "Geschakelde woning"];
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget> [
-        Container(
-          padding: const EdgeInsets.only(left: 30.0, right: 30, top: 5),
-          alignment: Alignment.centerLeft,
-          child: const Text(
-            "Cannenburgh 1, 1018 LG Amsterdam", 
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              color: Colors.grey,
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget> [
+          Container(
+            padding: const EdgeInsets.only(left: 30.0, right: 30, top: 5),
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              "Cannenburgh 1, 1018 LG Amsterdam", 
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: Colors.grey,
+              ),
             ),
           ),
-        ),
-        Container(
-          padding: const EdgeInsets.only(left: 30.0, right: 30, top: 5),
-          alignment: Alignment.centerLeft,
-          child: const Text(
-            "What type of property?", 
-            style: TextStyle(
-              fontWeight: FontWeight.w900,
-              fontSize: 24,
+          Container(
+            padding: const EdgeInsets.only(left: 30.0, right: 30, top: 5),
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              "What type of property?", 
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 24,
+              ),
             ),
           ),
-        ),
-        const SizedBox(height: 10),
-        Column(
-          children: [
-            apartmentOption(context),
-            
-          ],
-        ),
-      ],
+          const SizedBox(height: 10),
+          Column(
+            children: [
+              propertyType(context, propertyTypeList[0]),
+              propertyType(context, propertyTypeList[1]),
+              propertyType(context, propertyTypeList[2]),
+              propertyType(context, propertyTypeList[3]),
+              propertyType(context, propertyTypeList[4]),
+              propertyType(context, propertyTypeList[5]),
+              propertyType(context, propertyTypeList[6]),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
-  GestureDetector apartmentOption(BuildContext context) {
+  GestureDetector propertyType(BuildContext context, String proprtyTypeAtIndex) {
     return GestureDetector(
       onTap: () { 
-        print("Tapped a Container"); 
+        print("Tapped $proprtyTypeAtIndex"); 
       },
       child: Container(
+        margin: const EdgeInsets.only(bottom: 14.0),
         width: MediaQuery.of(context).size.width * 0.8,
         height: MediaQuery.of(context).size.height * 0.1,
         decoration: BoxDecoration(
@@ -65,9 +75,9 @@ class PropertyQuestionPage extends StatelessWidget {
                 painter: CirclePainter(),
               ),
               const SizedBox(width: 14,),
-              const Text(
-                "Apartment",
-                style: TextStyle(
+              Text(
+                proprtyTypeAtIndex,
+                style: const TextStyle(
                   color: Color.fromRGBO(101,101,107, 1),
                   fontSize: 14,
                 ),
