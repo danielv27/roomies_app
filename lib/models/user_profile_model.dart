046ba12f3ff1eed49016cf2aa0f79e1eab92ee45
dvolpin29 <d.volpin@student.vu.nl>
2022-07-11 23:52:33 +0200

    updated user_profile_model to hold all imagURLS and added UserModel

--- lib/models/user_profile_model.dart
+++ lib/models/user_profile_model.dart
@@ -1,15 +1,11 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'user_model.dart';
 
 class UserProfileModel {
-  final String id;
-  final String imageName;
-  final String imageUrl;
-  final String firstImgUrl; 
+  final UserModel userModel;
+  final List<String> imageURLS;
 
   UserProfileModel({
-    required this.id,
-    required this.imageName,
-    required this.imageUrl,
-    this.firstImgUrl = 'http://www.classicaloasis.com/wp-content/uploads/2014/03/profile-square.jpg'
+    required this.userModel,
+    required this.imageURLS,
   });
 }
