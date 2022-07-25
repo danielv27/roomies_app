e8d8b933ec342697d6a91360f51d4d334e21da7f
dvolpin29 <d.volpin@student.vu.nl>
2022-07-25 15:24:48 +0200

    added loading indicators for each widget component on the owner page

--- lib/widgets/owner_page/owner_house.dart
+++ lib/widgets/owner_page/owner_house.dart
@@ -21,64 +21,66 @@ class _OwnerHouseState extends State<ListedOwnerHouse> {
   Widget build(BuildContext context) {
     final HouseSignupProfileModel? houseProfile = widget.houseProvider.currentUser?.houseSignupProfileModel;
 
-    return Container(
-      margin: const EdgeInsets.only(bottom: 15.0),
-      child: Container(
+    return (houseProfile == null) 
+    ? const Center(child: CircularProgressIndicator(color: Colors.blue)) 
+    : Container(
         margin: const EdgeInsets.only(bottom: 15.0),
-        height: 100,
-        decoration: BoxDecoration(
-          color: const Color.fromRGBO(238, 238, 238, 1),
-          borderRadius: BorderRadius.circular(14.0),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Row(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(18),
-                height: 68,
-                width: 68,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(14.0),
-                  color: Colors.white,
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 15.0),
+          height: 100,
+          decoration: BoxDecoration(
+            color: const Color.fromRGBO(238, 238, 238, 1),
+            borderRadius: BorderRadius.circular(14.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(18),
+                  height: 68,
+                  width: 68,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14.0),
+                    color: Colors.white,
+                  ),
+                  child: Image.asset("assets/icons/Grey-house-selected.png",
+                    height: 20,
+                    width: 20,
+                  ),
                 ),
-                child: Image.asset("assets/icons/Grey-house-selected.png",
-                  height: 20,
-                  width: 20,
+                const SizedBox(
+                  width: 14,
                 ),
-              ),
-              const SizedBox(
-                width: 14,
-              ),
-              RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "${houseProfile?.postalCode}, ${houseProfile?.houseNumber}\n",
-                      style: const TextStyle(
-                        height: 2.0,
-                        color:Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w700,
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "${houseProfile.postalCode}, ${houseProfile.houseNumber}\n",
+                        style: const TextStyle(
+                          height: 2.0,
+                          color:Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
-                    ),
-                    TextSpan(
-                      text: "Matches", // TODO: Add number of matched houses
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w700,
-                        foreground: Paint()..shader = blueGradient().createShader(const Rect.fromLTWH(0.0, 0.0, 200.0, 100.0))
+                      TextSpan(
+                        text: "Matches", // TODO: Add number of matched houses
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          foreground: Paint()..shader = blueGradient().createShader(const Rect.fromLTWH(0.0, 0.0, 200.0, 100.0))
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Spacer(),
-              const Icon(Icons.arrow_forward_ios)
-            ],
+                const Spacer(),
+                const Icon(Icons.arrow_forward_ios)
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
--- lib/widgets/owner_page/owner_info.dart
+++ lib/widgets/owner_page/owner_info.dart
@@ -21,42 +21,44 @@ class _HouseOwnerContactInformationState extends State<HouseOwnerContactInformat
   Widget build(BuildContext context) {
     final HouseSignupProfileModel? houseProfile = widget.houseProvider.currentUser?.houseSignupProfileModel;
 
-    return Container(
-      margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.125,
-        decoration: BoxDecoration(
-          color: const Color.fromRGBO(245, 247, 251, 1),
-          borderRadius: BorderRadius.circular(14.0),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Row(
-            children: [
-              Image.asset("assets/icons/Email.png",
-                height: 20,
-                width: 20,
-              ),
-              const SizedBox(
-                width: 14,
-              ),
-              Expanded(
-                child: Text(
-                  "Name: ${houseProfile?.contactName}\nEmail: ${houseProfile?.contactEmail}\nTel: ${houseProfile?.contactPhoneNumber}",
-                  style: const TextStyle(
-                    color:Color.fromRGBO(101, 101, 107, 1),
-                    fontSize: 14,
-                    fontWeight: FontWeight.w300,
-                  ),
-                  softWrap: true,
-                  maxLines: 3,
-                  overflow: TextOverflow.ellipsis,
+    return (houseProfile == null) 
+    ? const Center(child: CircularProgressIndicator(color: Colors.blue)) 
+    : Container(
+        margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.125,
+          decoration: BoxDecoration(
+            color: const Color.fromRGBO(245, 247, 251, 1),
+            borderRadius: BorderRadius.circular(14.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Image.asset("assets/icons/Email.png",
+                  height: 20,
+                  width: 20,
                 ),
-              ),
-            ],
+                const SizedBox(
+                  width: 14,
+                ),
+                Expanded(
+                  child: Text(
+                    "Name: ${houseProfile.contactName}\nEmail: ${houseProfile.contactEmail}\nTel: ${houseProfile.contactPhoneNumber}",
+                    style: const TextStyle(
+                      color:Color.fromRGBO(101, 101, 107, 1),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w300,
+                    ),
+                    softWrap: true,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
