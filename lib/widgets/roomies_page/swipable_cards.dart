9ed4ea348a35026c0fc9aa748da0fa8fd01970e7
dvolpin29 <d.volpin@student.vu.nl>
2022-08-05 19:43:59 +0200

    fixed text overflow on the swipable cards when the location name is too long

--- lib/widgets/roomies_page/swipable_cards.dart
+++ lib/widgets/roomies_page/swipable_cards.dart
@@ -195,7 +195,7 @@ class UserInformation extends StatelessWidget {
     final user = userProfileModels![currentUserIndex].userModel;
 
     return Padding(
-      padding: const EdgeInsets.only(left: 25, bottom: 125.0),
+      padding: const EdgeInsets.only(left: 25, bottom: 125.0, right: 25),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -210,15 +210,22 @@ class UserInformation extends StatelessWidget {
           ),
           const SizedBox(height: 10,),
           Row(
+            mainAxisSize: MainAxisSize.min,
             children: [
               Image.asset("assets/icons/Location.png", width: 18, height: 18,),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: Text(
-                  user.location,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8),
+                  child: Text(
+                    user.location,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                    softWrap: false,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
